88e65d8a9ac0998893caa3cc3e25bc50bb0d6fcb
weekahrin <weekahrin@yandex.ru>
2026-07-22 10:52:27 +0300

    Refactor blur.hpp for C++23 & multiplatform support

--- blur.hpp
+++ blur.hpp
@@ -11,120 +11,105 @@
 #  include "imgui.h"
 #endif
 
-enum class Hardware : uint8_t {
+enum class Hardware {
     GPU,
     CPU
 };
 
 class Blur {
 public:
-    enum class Type : uint8_t {
-        Default   = 0,
-        Gaussian  = 1,
-        Box       = 2,
-        Frosted   = 3,
-        Kawase    = 4,
-        Bokeh     = 5,
-        Motion    = 6,
-        Zoom      = 7,
-        Pixelate  = 8
+    enum class Type {
+        Default = 0,
+        Gaussian = 1,
+        Box = 2,
+        Frosted = 3,
+        Kawase = 4,
+        Bokeh = 5,
+        Motion = 6,
+        Zoom = 7,
+        Pixelate = 8
     };
 
-    struct Rect {
-        int   X         = 0;
-        int   Y         = 0;
-        int   W         = 0;
-        int   H         = 0;
-        float Radius    = 12.0f;
-        int   Downscale = 4;
-    };
-
-    explicit Blur(Hardware backend = Hardware::GPU) noexcept;
+    explicit Blur(Hardware backend_in = Hardware::GPU);
     ~Blur();
 
-    Blur(const Blur&)            = delete;
-    Blur& operator=(const Blur&) = delete;
-    Blur(Blur&&)                 = delete;
-    Blur& operator=(Blur&&)      = delete;
-
-    void Process(int posX, int posY, int widthVal, int heightVal, float radiusVal, int downscaleVal);
-    void Process(const Rect& rectVal);
-
-    void process(int posX, int posY, int widthVal, int heightVal, float radiusVal, int downscaleVal) {
-        Process(posX, posY, widthVal, heightVal, radiusVal, downscaleVal);
-    }
-    void process(const Rect& rectVal) { Process(rectVal); }
+    void process(int x, int y, int w, int h, float radius, int downscale);
+    struct Rect {
+        int x = 0;
+        int y = 0;
+        int w = 0;
+        int h = 0;
+        float radius = 12.0f;
+        int downscale = 4;
+    };
+    void process(const Rect& r);
 
 #if defined(IMGUI_VERSION) && !defined(BLUR_NO_IMGUI)
-    void Process(ImDrawList* draw, float cornerRadius, float radiusVal, int downscaleVal, Hardware hw);
-    void Process(const ImRect& rectVal, float radiusVal, int downscaleVal);
-    void Process(const ImVec2& posVal, const ImVec2& sizeVal, float radiusVal, int downscaleVal);
-
-    void process(ImDrawList* draw, float cornerRadius, float radiusVal, int downscaleVal, Hardware hw) {
-        Process(draw, cornerRadius, radiusVal, downscaleVal, hw);
-    }
-    void process(const ImRect& rectVal, float radiusVal, int downscaleVal) {
-        Process(rectVal, radiusVal, downscaleVal);
-    }
-    void process(const ImVec2& posVal, const ImVec2& sizeVal, float radiusVal, int downscaleVal) {
-        Process(posVal, sizeVal, radiusVal, downscaleVal);
-    }
+    void process(ImDrawList* draw, float corner_radius, float radius, int downscale, Hardware hw);
+    void process(const ImRect& rect, float radius, int downscale);
+    void process(const ImVec2& pos, const ImVec2& size, float radius, int downscale);
 #endif
 
-    void Release();
-    void Reset();
+    void process(int x, int y, int w, int h, float radius, int downscale, Type t) {
+        type = t;
+        process(x, y, w, h, radius, downscale);
+    }
 
-    void release() { Release(); }
-    void reset() { Reset(); }
+    void Process(int x, int y, int w, int h, float radius, int downscale) { process(x, y, w, h, radius, downscale); }
+    void Process(const Rect& r) { process(r); }
 
-    GLuint   Texture  = 0;
-    GLuint   tex      = 0;
-    Hardware Backend  = Hardware::GPU;
-    Type     BlurType = Type::Default;
+    void release();
+    void reset();
+
+    void Release() { release(); }
+    void Reset() { reset(); }
+
+    GLuint tex = 0;
+    Hardware backend = Hardware::GPU;
+    Type type = Type::Default;
 
 private:
-    void Ensure();
-    void Apply();
+    void ensure();
+    void apply();
 
-    friend void GpuEnsure (Blur& t);
-    friend void GpuApply  (Blur& t);
-    friend void GpuRelease(Blur& t);
+    friend void gpuEnsure(Blur& t);
+    friend void gpuApply(Blur& t);
+    friend void gpuRelease(Blur& t);
 
-    friend void CpuApply(std::span<const uint8_t> input,
-                         std::span<uint8_t>       output,
-                         int widthVal, int heightVal,
-                         float radiusVal, int downscaleVal, int typeVal);
+    friend void cpuApply(std::span<const uint8_t> input,
+                         std::span<uint8_t> output,
+                         int w, int h, float radius, int downscale, int type);
 
-    int   x         = 0;
-    int   y         = 0;
-    int   w         = 0;
-    int   h         = 0;
-    float radius    = 12.0f;
-    int   downscale = 4;
+    int x_ = 0;
+    int y_ = 0;
+    int w_ = 0;
+    int h_ = 0;
+    float radius_ = 12.0f;
+    int downscale_ = 4;
 
-    int   width     = 0;
-    int   height    = 0;
+    int width_ = 0;
+    int height_ = 0;
 
-    std::unique_ptr<uint8_t[]> cpuInput;
-    std::unique_ptr<uint8_t[]> cpuOutput;
-    std::size_t                cpuSize   = 0;
+    std::unique_ptr<uint8_t[]> cpu_input_;
+    std::unique_ptr<uint8_t[]> cpu_output_;
+    size_t cpu_size_ = 0;
 
-    GLuint srcTex         = 0;
-    GLuint pingTex        = 0;
-    GLuint pongTex        = 0;
-    GLuint fbo            = 0;
-    GLuint vao            = 0;
-    GLuint vbo            = 0;
-    GLuint blurProgram    = 0;
-    GLuint blitProgram    = 0;
-    GLint  locTex         = -1;
-    GLint  locDirection   = -1;
-    GLint  locTexel       = -1;
-    GLint  locBlitTex     = -1;
-    GLint  locStrength    = -1;
-    int    blurW          = 0;
-    int    blurH          = 0;
-    bool   gpuReady       = false;
-    bool   cpuReady       = false;
-    Type   gpuProgramType = Type::Default;
+    GLuint src_tex_ = 0;
+    GLuint ping_tex_ = 0;
+    GLuint pong_tex_ = 0;
+    GLuint fbo_ = 0;
+    GLuint vao_ = 0;
+    GLuint vbo_ = 0;
+    GLuint blur_program_ = 0;
+    GLuint blit_program_ = 0;
+    GLint loc_tex_ = -1;
+    GLint loc_direction_ = -1;
+    GLint loc_texel_ = -1;
+    GLint loc_blit_tex_ = -1;
+    GLint loc_strength_ = -1;
+    int blur_w_ = 0;
+    int blur_h_ = 0;
+    bool gpu_ready_ = false;
+    bool cpu_ready_ = false;
+    Type gpu_program_type_ = Type::Default;
 };
